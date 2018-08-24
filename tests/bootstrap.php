f33bec9606ace5174ca35739d75a66a2a0b3caf9
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-08-24 23:43:35 +0900

    remove tests/bootstrap.php

--- tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$loader = require dirname(__DIR__) . '/vendor/autoload.php';
-/* @var $loader \Composer\Autoload\ClassLoader */
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
