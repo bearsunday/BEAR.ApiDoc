8cded3ec379a7c8c8f2a28443179d51af75871a1
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-09-03 03:33:54 +0900

    optional title

--- src/Curie.php
+++ src/Curie.php
@@ -13,7 +13,7 @@ final class Curie
     {
         $this->nameRel = $nameRel;
         $this->href = $curie['href'];
-        $this->title = $curie['title'];
+        $this->title = $curie['title'] ?? '';
         $this->rel = str_replace($curies->name . ':', '', $nameRel);
         $this->docUri = uri_template($curies->href, ['rel' => $this->rel]);
     }
