87ddf9b5a27e79e978d6c636ccd653259639e264
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-09-04 03:57:10 +0900

    add doc test

--- tests/DocGenTest.php
+++ tests/DocGenTest.php
@@ -3,11 +3,18 @@ namespace BEAR\ApiDoc;
 namespace BEAR\ApiDoc;
 
 use PHPUnit\Framework\TestCase;
+use function file_get_contents;
 
 class DocGenTest extends TestCase
 {
     public function test__invoke()
     {
         (new DocGen)('FakeVendor\FakeProject', __DIR__ . '/Fake/app', 'doc/api');
+        $this->assertFileExists(__DIR__ . '/doc/api/index.html');
+        $this->assertFileExists(__DIR__ . '/doc/api/rels/address.html');
+        $this->assertFileExists(__DIR__ . '/doc/api/schema/address.json');
+        $this->assertContains('<li><a href="rels/ticket.html">ticket</a>', file_get_contents(__DIR__ . '/doc/api/index.html'));
+        $this->assertContains('<h2>/address</h2>', file_get_contents(__DIR__ . '/doc/api/rels/address.html'));
+        $this->assertContains('$id": "address.json', file_get_contents(__DIR__ . '/doc/api/schema/address.json'));
     }
 }
