da58c2993fe1153528503a13130da467c29c9baa
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-09-07 12:44:58 +0900

    add testApiDocCommand

--- tests/DocGenTest.php
+++ tests/DocGenTest.php
@@ -3,7 +3,9 @@ namespace BEAR\ApiDoc;
 namespace BEAR\ApiDoc;
 
 use PHPUnit\Framework\TestCase;
+use function dirname;
 use function file_get_contents;
+use function passthru;
 
 class DocGenTest extends TestCase
 {
@@ -16,5 +18,16 @@ class DocGenTest extends TestCase
         $this->assertContains('<li><a href="rels/ticket.html">ticket</a>', file_get_contents(__DIR__ . '/doc/api/index.html'));
         $this->assertContains('<h2>/address</h2>', file_get_contents(__DIR__ . '/doc/api/rels/address.html'));
         $this->assertContains('$id": "http://example.com/schema/address.json', file_get_contents(__DIR__ . '/doc/api/schema/address.json'));
+    }
+
+    public function testApiDocCommand()
+    {
+        $appDir = dirname(__DIR__); // for autoloader
+        $bin = $appDir . '/bin/bear.apidoc';
+        $appName = "'FakeVendor\FakeProject'";
+        $docDir = __DIR__ . '/api_html';
+        $command = "$bin $appName $appDir $docDir";
+        passthru($command, $exitCode);
+        $this->assertSame(0, $exitCode);
     }
 }
