45479720cf6f5cea9dddf4e4d7be75bbe343a654
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-06-24 05:09:07 +0900

    convert URI parameter camelCase to snakeCase

---
+++ src/SnakeCaseExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+namespace BEAR\ApiDoc;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+final class SnakeCaseExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('snakecase', [$this, 'snakecase'])
+        ];
+    }
+
+    public function snakecase(string $string) : string
+    {
+        return ltrim(strtolower((string) preg_replace('/[A-Z]/', '_\0', $string)), '_');
+    }
+}
