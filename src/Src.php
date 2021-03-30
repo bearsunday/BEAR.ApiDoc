79d95294d7dfd78e90648e8cfb9dd02c05ed4f52
koriym <akihito.koriyama@gmail.com>
2021-03-30 11:08:14 +0900

    Came case for link document path

--- src/Src.php
+++ src/Src.php
@@ -8,8 +8,11 @@ use Rize\UriTemplate;
 use Rize\UriTemplate\Node\Literal;
 
 use function assert;
+use function ltrim;
 use function parse_url;
+use function preg_replace;
 use function sprintf;
+use function strtolower;
 use function substr;
 
 final class Src
@@ -32,8 +35,13 @@ final class Src
         $token = $literal->getToken();
         $parseUrl = parse_url(substr($token, 1));
         assert(isset($parseUrl['path']));
-        $resourcePath = $parseUrl['path'];
+        $resourcePath = substr($parseUrl['path'], 1);
 
-        return sprintf('[%s](%s.md)', $this->src, $resourcePath);
+        return sprintf('[<code>%s</code>](%s.md)', $this->src, $this->camel2kebab($resourcePath));
+    }
+
+    private function camel2kebab(string $str): string
+    {
+        return ltrim(strtolower((string) preg_replace('/[A-Z]/', '-\0', $str)), '-');
     }
 }
