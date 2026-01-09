d182fb38ede8d9307a406001b2d40fec35cae2d5
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-01-09 16:14:54 +0900

    Add helpful error message when config not found

--- src/Exception/ConfigNotFoundException.php
+++ src/Exception/ConfigNotFoundException.php
@@ -6,6 +6,16 @@ namespace BEAR\ApiDoc\Exception;
 
 use RuntimeException;
 
+use function sprintf;
+
 final class ConfigNotFoundException extends RuntimeException
 {
+    public function __construct(string $path)
+    {
+        $message = $path !== ''
+            ? sprintf("%s\nRun 'apidoc init' to create apidoc.xml", $path)
+            : "Run 'apidoc init' to create apidoc.xml";
+
+        parent::__construct($message);
+    }
 }
