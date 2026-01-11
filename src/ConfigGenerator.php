d7f40828aa05732ed3d0f3c3f5c87f3aaf090d68
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-01-11 12:34:45 +0900

    Remove alps from format comment (not a valid format option)

--- src/ConfigGenerator.php
+++ src/ConfigGenerator.php
@@ -31,7 +31,7 @@ final class ConfigGenerator
     <appName>%s</appName>
     <scheme>app</scheme>
     <docDir>docs/api</docDir>
-    <!-- format: html | md | openapi | alps | llms -->
+    <!-- format: html | md | openapi | llms -->
     <format>html</format>
     <title>%s API Doc</title>%s
     <links>
