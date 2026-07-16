65b75b84bd5af40751301b745ebcc6eeb3c5799e
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-07-16 13:23:31 +0900

    Cap object graph search zoom

--- docs/assets/bindings-object-graph.js
+++ docs/assets/bindings-object-graph.js
@@ -31,6 +31,7 @@
   }
 
   function enableSearch(svg) {
+    var maxZoom = 6;
     var originalViewBox = svg.getAttribute('viewBox');
     var originalBounds = originalViewBox ? originalViewBox.split(/[,\s]+/).map(Number) : null;
     var nodes = Array.from(svg.querySelectorAll('g.node'));
@@ -123,6 +124,25 @@
         var fittedWidth = height * viewportRatio;
         minX -= (fittedWidth - width) / 2;
         width = fittedWidth;
+      }
+      if (originalBounds) {
+        var scale = Math.max(
+          originalBounds[2] / maxZoom / width,
+          originalBounds[3] / maxZoom / height,
+          1
+        );
+        var centerX = minX + width / 2;
+        var centerY = minY + height / 2;
+        width *= scale;
+        height *= scale;
+        minX = centerX - width / 2;
+        minY = centerY - height / 2;
+        if (width < originalBounds[2]) {
+          minX = Math.min(Math.max(minX, originalBounds[0]), originalBounds[0] + originalBounds[2] - width);
+        }
+        if (height < originalBounds[3]) {
+          minY = Math.min(Math.max(minY, originalBounds[1]), originalBounds[1] + originalBounds[3] - height);
+        }
       }
       svg.setAttribute('viewBox', [minX, minY, width, height].join(' '));
       mount.classList.add('is-focused');
